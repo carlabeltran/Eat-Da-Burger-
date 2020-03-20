4bf6e0a428c613206e9b652bea3f4c5be8ada0b9
carlabeltran <carlabeltran14@icloud.com>
2020-03-20 11:48:42 -0500

    updated burger.js file, corrected controller file name, corrected css file name

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,29 @@
 //IMPORT ORM TO CREATE FUNCTIONS THAT WILL INTERACT W/ DATABASE
 var orm = require("../config/orm.js");
 
-var cat = {
+var burgers = {
   all: function(cb) {
-    orm.all("cats", function(res) {
+    orm.all("burgers", function(res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
+  //ARRAY VARIABLE COLS & VALS.
   create: function(cols, vals, cb) {
-    orm.create("cats", cols, vals, function(res) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("cats", objColVals, condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
+    orm.delete("burgers", condition, function(res) {
       cb(res);
     });
   }
 };
 
-// Export the database functions for the controller (catsController.js).
+//EXPORT DB FUNCTIONS FOR THE CONTROLLER BURGER Export the database functions for the controller (catsController.js).
 module.exports = cat;
