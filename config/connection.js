d0509ef5f4d2f3d6c18fe37ec8fc2edfd85d041b
carlabeltran <carlabeltran14@icloud.com>
2020-03-24 05:02:16 -0500

    Successfull Heroku Deployed Application

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,22 @@
 require("dotenv").config();
 //SET UP MYSQL CONNECTION
 var mysql = require("mysql");
-let connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
   //CONNECT TO JAWSDB(HEROKU)
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+  connection.connect();
+
+  connection.query("SELECT 1 + 1 AS solution", function (err, rows, fields) {
+    if (err) throw err;
+
+    console.log("The solution is: ", rows[0].solution);
+  });
+
+  connection.end();
+
 } else {
 
   //connect to localhost
@@ -16,15 +27,16 @@ if (process.env.JAWSDB_URL) {
     password: process.env.PASS,
     database: "burgers_db"
   });
+
+  //MAKE CONNECTION
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 }
-//MAKE CONNECTION
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 //EXPORT CONNECTION FOR OUR ORM TO USE
 module.exports = connection;
